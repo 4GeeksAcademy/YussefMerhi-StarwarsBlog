fcbf7839b354bcc03dc8c3d3bd960ec6ec8a162f
Yussef Merhi <101605162+Annorakx@users.noreply.github.com>
2024-06-06 20:20:40 +0000

    funciones en el flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,69 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			starships: [],
+			planets: [],
+			species: [],
+			vehicles: [],
+			films: [],
+			favorites: [],
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getCharacters: () => {
+				fetch("https://www.swapi.tech/api/people/")
+				.then(response => response.json())
+				.then(data => {
+					setStore({ characters: data.results});
+				});
 			},
+			getStarships: () => {
+				fetch("https://www.swapi.tech/api/starships/")
+				.then(response => response.json())
+				.then(data => {
+					setStore({ starships: data.results });
+				});
+			},
+			getPlanets: () => {
+				fetch("https://www.swapi.tech/api/planets/")
+				.then(response => response.json())
+				.then(data => {
+					setStore({ planets: data.results });
+				});
+			},
+			getSpecies: () => {
+				fetch("https://www.swapi.tech/api/species/")
+				.then(response => response.json())
+				.then(data => {
+					setStore({ species: data.results });
+				});
+			},
+			getVehicles: () => {
+				fetch("https://www.swapi.tech/api/vehicles/")
+				.then(response => response.json())
+				.then(data => {
+					setStore({ vehicles: data.results });
+				});
+			},
+			getFilms: () => {
+				fetch("https://www.swapi.tech/api/films/")
+				.then(response => response.json())
+				.then(data => {
+					setStore({ films: data.results });
+				});
+			},
+			getUID: (category) => {
+				return getStore()[category].map(element => { 
+					 let urlArr = element.url?.split("/");
+					 return urlArr[urlArr.length - 2];
+					});
+			}, 
 			loadSomeData: () => {
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
