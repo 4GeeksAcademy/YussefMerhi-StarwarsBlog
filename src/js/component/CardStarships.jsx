4e1a16aebd74ac8730a527cac70fc36308f10d8d
Yussef Merhi <101605162+Annorakx@users.noreply.github.com>
2024-06-11 13:53:33 +0000

    se resolvieron algunos errores de consola web.

--- src/js/component/CardStarships.jsx
+++ src/js/component/CardStarships.jsx
@@ -4,18 +4,22 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 const CardStarships = () => {
-    const { store, actions } = useContext(Context)
-    let storeFavorites = store.favorites
+    const { store, actions } = useContext(Context);
+    let storeFavorites = store.favorites;
+
     return (
         <>
             {store.starships.map((value, index) => (
-
-                <div>
+                <div key={value.uid}>
                     <div className="card" style={{ width: "18rem" }}>
                         <img
-                            src={`${value.uid == 2 || value.uid == 3 || value.uid == 17 ? "https://starwars-visualguide.com/assets/img/placeholder.jpg"
-                                : `https://starwars-visualguide.com/assets/img/starships/${value.uid}.jpg`}`}
-                            className="img-size" />
+                            src={`${value.uid == 2 || value.uid == 3 || value.uid == 17
+                                ? "https://starwars-visualguide.com/assets/img/placeholder.jpg"
+                                : `https://starwars-visualguide.com/assets/img/starships/${value.uid}.jpg`
+                                }`}
+                            className="img-size"
+                            alt={value.name}
+                        />
 
                         <div className="card-body bg-dark text-light">
                             <h5 className="card-title">{value.name}</h5>
@@ -23,17 +27,22 @@ const CardStarships = () => {
                                 <Link to={`/singlestarshippage/${value.uid}`}>
                                     <button className="btn btn-primary">Learn more!</button>
                                 </Link>
-                                <button className={`btn btn-warning border-0 text-light  ${storeFavorites.includes(value.name) ? "disabled" : ""}`} onClick={() => {
-                                    actions.getFavorite(value.name)
-                                }}><i className="fa-solid fa-heart"></i></button>
+                                <button
+                                    className={`btn btn-warning border-0 text-light  ${storeFavorites.includes(value.name) ? "disabled" : ""
+                                        }`}
+                                    onClick={() => {
+                                        actions.getFavorite(value.name);
+                                    }}
+                                >
+                                    <i className="fa-solid fa-heart"></i>
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
             ))}
         </>
+    );
+};
 
-    )
-}
-
-export default CardStarships
+export default CardStarships;
